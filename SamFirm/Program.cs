821139454e87be9918f4520faa9555a73c01b621
Lulin Pollux <kistssy+dev@gmail.com>
2019-01-23 23:38:21 +0900

    매개변수 개수확인 삭제

--- SamFirm/Program.cs
+++ SamFirm/Program.cs
@@ -6,16 +6,12 @@ namespace SamFirm
     internal static class Program
     {
         [STAThread]
-        private static int Main(string[] args)
+        static void Main()
         {
-            if (args.Length == 0)
-            {
-                Imports.FreeConsole();
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
-            }
-            return 0;
+            Imports.FreeConsole();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
