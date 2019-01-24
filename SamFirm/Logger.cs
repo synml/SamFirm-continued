3d07e315e27359977fc490b0aae062b9e3dc2d31
Lulin Pollux <kistssy+dev@gmail.com>
2019-01-25 00:50:58 +0900

    로그를 append에서 create로 변경

--- SamFirm/Logger.cs
+++ SamFirm/Logger.cs
@@ -8,24 +8,6 @@ namespace SamFirm
     {
         internal static Form1 form;
 
-        //로그 텍스트 박스의 줄 개수가 30개를 초과하면 지우는 메소드
-        private static void CleanLog()
-        {
-            if (form.log_textbox.InvokeRequired)
-            {
-                form.log_textbox.Invoke(new Action(delegate {
-                    if (form.log_textbox.Lines.Length > 30)
-                    {
-                        form.log_textbox.Text.Remove(0, form.log_textbox.GetFirstCharIndexFromLine(1));
-                    }
-                }));
-            }
-            else if (form.log_textbox.Lines.Length > 30)
-            {
-                form.log_textbox.Text.Remove(0, form.log_textbox.GetFirstCharIndexFromLine(1));
-            }
-        }
-
         //현재 날짜와 시각을 알아내는 함수
         private static string GetTimeDate()
         {
@@ -37,14 +19,8 @@ namespace SamFirm
         {
             if (!string.IsNullOrEmpty(form.log_textbox.Text))
             {
-                if (File.Exists("SamFirm.log") && (new FileInfo("SamFirm.log").Length > 0x200000L))
+                using (TextWriter writer = new StreamWriter(new FileStream("SamFirm.log", FileMode.Create)))
                 {
-                    File.Delete("SamFirm.log.old");
-                    File.Move("SamFirm.log", "SamFirm.log.old");
-                }
-                using (TextWriter writer = new StreamWriter(new FileStream("SamFirm.log", FileMode.Append)))
-                {
-                    writer.WriteLine();
                     writer.WriteLine(GetTimeDate());
                     foreach (string str in form.log_textbox.Lines)
                     {
@@ -58,8 +34,6 @@ namespace SamFirm
         public static void WriteLine(string str)
         {
             MethodInvoker method = null;
-
-            CleanLog();
 
             if (form.log_textbox.InvokeRequired)
             {
