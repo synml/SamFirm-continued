7c199e0c29f3223787a0749bc9e1febdd8dd095e
Lulin Pollux <kistssy+dev@gmail.com>
2019-01-25 00:22:45 +0900

    리팩토링
    1. 사용하지 않는 매개변수 제거함.

--- SamFirm/Form1.cs
+++ SamFirm/Form1.cs
@@ -723,7 +723,7 @@ namespace SamFirm
                     }
                     Utility.TaskBarProgressState(false);
                     BackgroundWorker worker = new BackgroundWorker();
-                    worker.DoWork += delegate (object o, DoWorkEventArgs _e) {
+                    worker.DoWork += delegate {
                         MethodInvoker method = null;
                         MethodInvoker invoker2 = null;
                         MethodInvoker invoker3 = null;
@@ -833,8 +833,7 @@ namespace SamFirm
 
             //백그라운드 작업 등록
             BackgroundWorker worker = new BackgroundWorker();
-            worker.DoWork += delegate (object o, DoWorkEventArgs _e)
-            {
+            worker.DoWork += delegate {
                 MethodInvoker method = null;
                 MethodInvoker invoker2 = null;
                 MethodInvoker invoker3 = null;
