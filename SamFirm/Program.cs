514e36c2c0d7cf0086ff4aa8252a2f90279dc480
Lulin Pollux <kistssy+dev@gmail.com>
2019-07-29 17:18:06 +0900

    main: add culture setting
    디버그 모드에서만 이탈리아 문화권으로 설정함.

--- SamFirm/Program.cs
+++ SamFirm/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Reflection;
 using System.Windows.Forms;
 
@@ -13,6 +14,11 @@ namespace SamFirm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Imports.FreeConsole();
+
+#if DEBUG
+            //문화권을 이탈리아로 설정한다.
+            CultureInfo.CurrentUICulture = new CultureInfo("it");
+#endif
 
             //제목 표시줄에 버전정보를 출력한다.
             MainForm mainForm = new MainForm();
